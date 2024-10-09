10c4077b151474c1038ee512425563dfeb217cd1
Peter <47840148+Sweedsiter@users.noreply.github.com>
2024-10-09 09:41:04 +0000

    6

--- components/conteners/Home1.jsx
+++ components/conteners/Home1.jsx
@@ -13,19 +13,20 @@ export default function Home1() {
         loop
         autoPlay
         muted
-        src="https://videocdn.cdnpk.net/videos/6df7325c-b0c6-49da-a6ad-5fc4d1c3d1fd/horizontal/previews/clear/large.mp4?token=exp=1728463638~hmac=ae599bef0ae8b76524d8087a342becf402a9d407f8a26807b446b6c25bf64aca"
+        src="https://videocdn.cdnpk.net/videos/e5e20d0f-c29b-4fde-9cb3-7d0ebc0c2f7f/horizontal/previews/clear/large.mp4?token=exp=1728467661~hmac=8f140b5714b736faf230121098faa176d0f8f3e2c2fba9d79459aa042d08c86b"
         className="w-full"
       />
+
       <div className="desktop:absolute flex flex-row flex-wrap items-center  justify-center">
         <div className="p-3 text-left flex flex-col desktop:w-2/6">
           <h1 className="py-3 text-xl font-bold cursor-pointer duration-300 hover:text-orange-600 hover:text-2xl cursor-pointer">
-            Company Name
+            Hi My name is Peter
           </h1>
           <span className="hover:text-base desktop:hover:font-semibold duration-300 hover:text-slate-900 text-slate-800 cursor-pointer ">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio
-            enim nisi perspiciatis quo expedita, iste inventore voluptate unde
-            assumenda vitae veniam. Modi quae quas adipisci? Neque dolor magnam
-            illo minima.
+            สวัสดีครับ .... ผมชื่อ (ปีเตอร์) เป็นคนไทยใหญ่ ถึอสัญชาติพม่า
+            บ้านเกิดผมอยู่ที่ Hsipaw ประเทศ Myanmar หมู่บ้าน (village San Pake){" "}
+            <br />
+            เรียนที่ No.1 Basic Education High School Hsipaw
           </span>
           <button
             type="download"
