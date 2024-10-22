87c2b9415580768cdfdf471445dedb536ace9de6
Peter <47840148+Sweedsiter@users.noreply.github.com>
2024-10-22 07:49:21 +0000

    22/10/2024 V2

--- components/conteners/Embriodery.jsx
+++ components/conteners/Embriodery.jsx
@@ -1,18 +1,23 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { Data } from "@/public/Embroidery/Embroider";
 
 export default function Embroidery() {
   return (
-    <div className="w-full h-auto flex flex-row flex-wrap">
-      <Link href="/Embroidery/DLP02Print.jpg" target="_blank">
-        <Image
-          src="/Embroidery/DLP02Print.jpg"
-          width={500}
-          height={500}
-          alt="Picture of the author"
-          className="w-60"
-        />
-      </Link>
+    <div className="w-full h-auto flex desktop:flex-row flex-wrap justify-center desktop:px-3">
+      {Data.map((e) => (
+        <Link href={e.src} target="_blank" className="p-2">      
+          <Image
+            src={e.src}
+            width={300}
+            height={300}
+            alt="Picture of the author"
+            className="desktop:w-60 w-full"
+          />
+        </Link>
+      ))}
     </div>
   );
 }
---
+++ public/Embroidery/Embroider.js
@@ -0,0 +1,13 @@
+const Data = [
+  {
+    src: "/Embroidery/DLP02Print.jpg",
+  },
+  {
+    src: "/Embroidery/DLP02Print.jpg",
+  },
+  {
+    src: "/Embroidery/DLP02Print.jpg",
+  },
+];
+
+module.exports.Data = Data;
